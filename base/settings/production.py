8df6ab55c5f00c5e0b8eeb39d1d4318153ee062e
Penk13 <natanaelsteven109@gmail.com>
2022-05-08 22:42:35 +0700

    heroku postgresql

--- base/settings/production.py
+++ base/settings/production.py
@@ -1,4 +1,9 @@
 from .base import *
+import dj_database_url
+
 
 DEBUG = False
 ALLOWED_HOSTS = ['climatecare.herokuapp.com']
+
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
